7a185cabf83b8e7cd5d017ec91c7886ca6f012f3
TUNG LE <leesontungggg@gmail.com>
2024-08-24 15:39:10 +0700

    enhance email sent for order

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -2,9 +2,6 @@ export default function ThankyouPage() {
   return (
     <div className="w-screen min-h-screen h-fit flex items-center justify-center bg-black/70">
       <div className="container mx-auto text-white text-xl md:text-4xl leading-relaxed">
-        Mong bà con chịu khó chờ xíu nha
-        <br />
-        <br />
         Cảm ơn bà con đã đặt hàng và cổ động chúng tôi!
         <br />
         <br />
@@ -17,6 +14,9 @@ export default function ThankyouPage() {
         <br />
         <br />
         Link pre-order đợt này sẽ mở từ ngày 26/8/2024 tới ngày 9/9/2024
+        <br />
+        <br />
+        Mong bà con chịu khó chờ xíu nha
         <br />
         <br />
         🤘🤘🤘
