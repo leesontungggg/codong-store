7beb0fa06e2829e1b7e0d5041c68a6d682d51cd8
TUNG LE <leesontungggg@gmail.com>
2024-08-24 10:30:33 +0700

    asd

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,18 +1,23 @@
 export default function ThankyouPage() {
   return (
     <div className="w-screen min-h-screen h-fit flex items-center justify-center bg-black/70">
-      <div className="container mx-auto text-white text-xl md:text-4xl leading-loose">
+      <div className="container mx-auto text-white text-xl md:text-4xl leading-relaxed">
         Mong bà con chịu khó chờ xíu nha
         <br />
+        <br />
         Cảm ơn bà con đã đặt hàng và cổ động chúng tôi!
+        <br />
         <br />
         Đối với nón Cổ Động, mọi người sẽ nhận được hàng sau một tuần kể từ khi
         đóng link pre-order.
         <br />
+        <br />
         Đối với áo Cổ Động, mọi người sẽ nhận được hàng sau 7 ngày kể từ khi
         đóng link pre-order.
         <br />
+        <br />
         Link pre-order đợt này sẽ mở từ ngày 26/8/2024 tới ngày 9/9/2024
+        <br />
         <br />
         🤘🤘🤘
         <br />
